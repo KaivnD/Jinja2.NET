a6b2d84694455011501c58ae658e0dd5149a9f84
KaivnD <37983330+KaivnD@users.noreply.github.com>
2025-12-23 12:24:53 +0800

    update chat template test status

--- Jinja2.NET.Tests/ChatTemplateTests.cs
+++ Jinja2.NET.Tests/ChatTemplateTests.cs
@@ -27,8 +27,8 @@ public class ChatTemplateTests
     [InlineData("gpt2")]
     [InlineData("llama")]
     [InlineData("whisper")]
-    [InlineData("HuggingFaceH4/zephyr-7b-beta (add_generation_prompt=false)")] // str differs
-    [InlineData("HuggingFaceH4/zephyr-7b-beta (add_generation_prompt=true)")] // str differs
+    [InlineData("HuggingFaceH4/zephyr-7b-beta (add_generation_prompt=false)")]
+    [InlineData("HuggingFaceH4/zephyr-7b-beta (add_generation_prompt=true)")]
     [InlineData("HuggingFaceH4/zephyr-7b-gemma-v0.1")]
     [InlineData("TheBloke/Mistral-7B-Instruct-v0.1-GPTQ")]
     [InlineData("mistralai/Mixtral-8x7B-Instruct-v0.1")]
@@ -36,16 +36,16 @@ public class ChatTemplateTests
     [InlineData("openchat/openchat-3.5-0106")]
     [InlineData("upstage/SOLAR-10.7B-Instruct-v1.0")]
     [InlineData("codellama/CodeLlama-70b-Instruct-hf")]
-    [InlineData("Deci/DeciLM-7B-instruct")] // str differs
+    [InlineData("Deci/DeciLM-7B-instruct")]
     [InlineData("Qwen/Qwen1.5-72B-Chat")]
     [InlineData("deepseek-ai/deepseek-llm-7b-chat")]
     [InlineData("h2oai/h2o-danube-1.8b-chat")]
     [InlineData("internlm/internlm2-chat-7b")]
-    [InlineData("TheBloke/deepseek-coder-33B-instruct-AWQ")] // str differs
+    [InlineData("TheBloke/deepseek-coder-33B-instruct-AWQ")]
     [InlineData("ericzzz/falcon-rw-1b-chat")] // indexing issue
     [InlineData("abacusai/Smaug-34B-v0.1")]
-    [InlineData("maywell/Synatra-Mixtral-8x7B")] // str differs
-    [InlineData("deepseek-ai/deepseek-coder-33b-instruct")] // str differs
+    [InlineData("maywell/Synatra-Mixtral-8x7B")]
+    [InlineData("deepseek-ai/deepseek-coder-33b-instruct")]
     [InlineData("meetkai/functionary-medium-v2.2")] // str differs
     [InlineData("fireworks-ai/firefunction-v1")] // parsing issue
     [InlineData("maywell/PiVoT-MoE")] // parsing issue
@@ -60,7 +60,7 @@ public class ChatTemplateTests
     [InlineData("MadeAgents/Hammer2.1")] // parsing issue
     [InlineData("Qwen/Qwen2.5-7B-Instruct")] // unknown
     [InlineData("Qwen/Qwen2.5-VL-7B-Instruct")] // binary operation issue
-    [InlineData("Qwen/Qwen3-0.6B")] // str differs
+    [InlineData("Qwen/Qwen3-0.6B")]
     [InlineData("CohereLabs/c4ai-command-a-03-2025")]  // parsing issue
     [InlineData("openbmb/MiniCPM3-4B")] // parsing issue
     [InlineData("ai21labs/AI21-Jamba-Large-1.6")]  // parsing issue
